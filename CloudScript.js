d16013f46b4ab1ded66d7bea791da2d9a1574bf1
romkij <romkiij@gmail.com>
2015-12-27 19:37:01 +0300

    test

---
+++ CloudScript.js
@@ -0,0 +1,23 @@
+handlers.newUserAction = function(args) {
+	var SettingsKey = "Settings";
+	var CharactersKey = "Characters";	
+	
+	var titleData = server.GetTitleData({
+		Keys : [ SettingsKey ]
+	});
+	
+	var startDragon = JSON.parse(titleData.Data[SettingsKey]).StartDragon;
+	
+	var grantResult = server.GrantItemsToUser({
+		PlayFabId: currentPlayerId,
+		ItemIds : [ "powerup_magnet", "powerup_shield", "powerup_multiplier" ]
+	});
+	
+	var result = server.GrantCharacterToUser({
+		PlayFabId : currentPlayerId,
+		CharacterName : startDragon.CharacterName,
+		CharacterType: startDragon.CharacterType
+	});
+	
+	return result;
+}
