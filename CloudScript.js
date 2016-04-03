c5a8118125a696817eb8188e77e08d89fde7c931
romkij <romkiij@gmail.com>
2016-04-03 21:42:03 +0300

    Add daily challenges support.

--- CloudScript.js
+++ CloudScript.js
@@ -31,3 +31,82 @@ handlers.grantUserItems = function(args) {
 	
 	return result;
 }
+
+function currentTimeInSeconds()
+{
+    var now = new Date().getUTCDate();
+    return now.getTime() / 1000;
+}
+
+handlers.requestDaily = function (args) {
+    var result;
+    var oneDay = 86400;
+
+    var requestDateTime = currentTimeInSeconds();
+    var completedDays = args.completedDays;
+
+	var playerInternalData = server.GetUserInternalData({
+		PlayFabId: currentPlayerId,
+		Keys: ["dailyCompletedDays", "dailyNextRequestTime", "dailyDeadlineDateTime"]
+	});
+
+    var deadlineDateTime = playerInternalData.Data["dailyDeadlineDateTime"];
+	var storedCompletedDays = playerInternalData.Data["dailyCompletedDays"];
+    var nextRequestDateTime = playerInternalData.Data["dailyNextRequestDateTime"];
+
+	if (!nextRequestDateTime)
+	{
+        // First request of daily.
+        deadlineDateTime = requestDateTime + oneDay; // request time in seconds + 1 day in seconds.
+        nextRequestDateTime = deadlineDateTime + oneDay;
+        storedCompletedDays = 0
+    }
+    else
+    {
+        deadlineDateTime = parseInt(deadlineDateTime.Value);
+        storedCompletedDays = parseInt(storedCompletedDays.Value);
+        nextRequestDateTime = parseInt(nextRequestDateTime.Value);
+
+        if (deadlineDateTime <= requestDateTime)
+        {
+            // Time to check.
+
+            if (nextRequestDateTime > requestDateTime)
+            {
+                // Good. Client need new level.
+
+                deadlineDateTime = nextRequestDateTime; // request time in seconds + 1 day in seconds.
+                nextRequestDateTime = deadlineDateTime + oneDay;
+                storedCompletedDays = completedDays;
+            }
+            else
+            {
+                // Bad. Too late to cry. Reset daily.
+
+                deadlineDateTime = requestDateTime + oneDay; // request time in seconds + 1 day in seconds.
+                nextRequestDateTime = deadlineDateTime + oneDay;
+                storedCompletedDays = 0
+            }
+        }
+        else
+        {
+            // Need to wait end of day for new daily.
+            storedCompletedDays = completedDays;
+        }
+    }
+
+    server.UpdateUserInternalData({
+        PlayFabId: currentPlayerId,
+        Data: {
+            dailyDeadlineDateTime : String(deadlineDateTime),
+            dailyCompletedDays : String(storedCompletedDays),
+            dailyNextRequestDateTime : String(nextRequestDateTime)
+        }
+    });
+
+	return {
+        deadline : deadlineDateTime,
+        completedDays : storedCompletedDays,
+        requestDeadline : nextRequestDateTime
+    };
+}
