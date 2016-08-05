a66ff0d0828b14770ef71f312e5c8d22ec14bcf2
romkij <romkiij@gmail.com>
2016-08-05 14:13:13 +0300

    Add bundle script

--- BundleScript.js
+++ BundleScript.js
@@ -7,4 +7,45 @@ handlers.getManifest = function (args) {
     var clientVersion = args.ClientVersion;
     var isDebug = args.IsDebug;
 
+    var internalData = server.GetTitleInternalData({
+        Keys: [key]
+    });
+
+    if (internalData.Data.hasOwnProperty(key)) {
+        internalData = JSON.parse(internalData.Data[key]);
+        var currentTimestamp = currentTimeInSeconds();
+
+        var manifests = internalData.Manifests.filter(function (manifest) {
+            if (isDebug) {
+                return manifest.ClientVersion == clientVersion;
+            }
+            else {
+                return manifest.ClientVersion == clientVersion && (manifest.CreatedTimestamp) + internalData.LiveOffset <= currentTimestamp;
+            }
+        });
+
+        manifests.sort(function (a, b) {
+            if (a.Revision > b.Revision)
+                return 1;
+            if (a.Revision < b.Revision)
+                return -1;
+            return 0;
+        });
+
+        if (manifests.length > 0) {
+            var manifest = manifests[manifests.length - 1];
+            var manifestKey = internalData.TemplatePath.replace('%client_version%', clientVersion).replace('%revision%', manifest.Revision);
+
+            var url = server.GetContentDownloadUrl({
+                Key: manifestKey,
+                ThruCDN: false
+            }).URL;
+
+            var request = http.request(url, "get", "", "application/json");
+            return JSON.parse(request);
+        }
+        else {
+            return null;
+        }
+    }
 };
