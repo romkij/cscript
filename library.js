fd1ea0053e5ed1b2a1f3d7b1a2fe531d27944481
romkij <romkiij@gmail.com>
2016-10-11 20:19:05 +0300

    Add platform selection.

---
+++ library.js
@@ -0,0 +1,71 @@
+/**
+ * Created by Romkij on 10/11/2016.
+ */
+
+// Additional functionality.
+function currentTimeInSeconds() {
+    var now = new Date();
+    return Math.floor(now.getTime() / 1000);
+}
+
+function guid() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    }
+
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
+}
+
+function getTitleData(key) {
+    var titleData = server.GetTitleData({
+        Keys: [key]
+    });
+
+    if (titleData.Data.hasOwnProperty(key))
+        return JSON.parse(titleData.Data[key])
+}
+
+function isEmpty(obj) {
+    return Object.keys(obj).length === 0 && JSON.stringify(obj) === JSON.stringify({});
+}
+
+
+function getCalculationType(fullname, settings) {
+    var statSettings = getSettingsByFullName(fullname, settings);
+
+    if (typeof statSettings != "undefined") {
+        return getCalculationTypeByFullName(fullname, statSettings.Info);
+    }
+}
+
+function getSettingsByFullName(fullName, settings) {
+    for (var i = 0; i < settings.length; i++) {
+        var stat = settings[i];
+
+        if (Contains(fullName, stat.Name))
+            return stat;
+    }
+}
+
+function getCalculationTypeByFullName(fullName, statInfo) {
+    for (var i = 0; i < statInfo.length; i++) {
+        var info = statInfo[i];
+
+        if (Contains(fullName, info.CollectionType))
+            return info.CalculateType;
+    }
+}
+
+function Contains(a, b) {
+    return a.indexOf(b) >= 0;
+}
+
+function isValid(data, hash) {
+    var json = JSON.stringify(data);
+
+    var words = CryptoJS.HmacMD5(JSON.stringify(args.data), currentPlayerId);
+    var hash = CryptoJS.enc.Base64.stringify(words);
+
+    return hash === args.hash;
+}
