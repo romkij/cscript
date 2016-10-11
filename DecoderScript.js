554d5e1af41e4e7c0d147213142d1462d9796a58
romkij <romkiij@gmail.com>
2016-10-11 17:11:54 +0300

    Add platform selection.

---
+++ DecoderScript.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Romkij on 10/11/2016.
+ */
+
+handlers.testEncryption = function (args) {
+    // var data = Base64.encode()args.data
+    return "valera mat' ego".toBase64();
+};
