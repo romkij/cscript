b533038649c589413a9699a5b51cf687e99371bb
romkij <romkiij@gmail.com>
2016-10-11 20:10:16 +0300

    Add platform selection.

--- DecoderScript.js
+++ DecoderScript.js
@@ -8,6 +8,14 @@ handlers.testEncryption = function (args) {
     var words = CryptoJS.HmacMD5(JSON.stringify(args.data), currentPlayerId);
     var hash = CryptoJS.enc.Base64.stringify(words);
 
+
+    var fdss = CryptoJS.enc.Base64.parse(hash);
+
+    return {
+        before: words,
+        after: fdss
+    };
+
     if (hash === args.hash);
 
     return true;
