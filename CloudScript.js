af22b99bb3654e0d20f358f169796c380b26bf26
romkij <romkiij@gmail.com>
2016-10-12 00:12:54 +0300

    Add platform selection.

--- CloudScript.js
+++ CloudScript.js
@@ -1,6 +1,8 @@
 handlers.newUserAction = function (args) {
     var SettingsKey = "Settings";
 
+    if (!isValid(args.Data, args.Hash))
+        log.debug('HASH INVALID!');
     var titleData = getTitleData(SettingsKey);
 
     var startDragon = titleData.StartDragon;
