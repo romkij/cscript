5ef180796a5580a1545a5431813576f1d409da25
romkij <romkiij@gmail.com>
2016-10-12 18:06:27 +0300

    Add platform selection.

--- CloudScript.js
+++ CloudScript.js
@@ -38,7 +38,7 @@ function checkTimestamp(action, timestamp) {
         Keys: [SECURITY_KEY]
     });
 
-    data = data.Data[SECURITY_KEY].Value;
+    data = JSON.parse(data.Data[SECURITY_KEY].Value);
 
     return data;
 }
